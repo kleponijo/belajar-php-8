7300142c1f531a0d4b84b35cb097b11a109c5b37
kleponijo <posapaaja@gmail.com>
2026-06-14 15:09:53 +0700

    Update Attribute.php

--- Attribute.php
+++ Attribute.php
@@ -6,10 +6,33 @@ class NotBlank {}
 class LoginRequest
 {
   #[NotBlank]
-  public string $username;
+  public ?string $username;
 
   #[NotBlank]
-  public string $password;
+  public ?string $password;
 }
 
-function validate(object $object): void {}
+function validate(object $object): void
+{
+  $class = new ReflectionClass($object);
+  $properties = $class->getProperties();
+  foreach ($properties as $property) {
+    validateNotBlank($property, $object);
+  }
+}
+
+function validateNotBlank(ReflectionProperty $property, object $object): void
+{
+  $attributes = $property->getAttributes(NotBlank::class);
+  if (count($attributes) > 0) {
+    if (!$property->isInitialized($object))
+      throw new Exception("Property $property->name is null");
+    if ($property->getValue($object) == null)
+      throw new Exception("Property $property->name is null");
+  }
+}
+
+$request = new LoginRequest();
+$request->username = "nadif";
+$request->password = null;
+validate($request);
