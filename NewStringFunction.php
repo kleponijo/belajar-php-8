c1036b8d267ec208a27329df6495fa17fd50664c
kleponijo <posapaaja@gmail.com>
2026-07-10 15:20:11 +0700

    Create NewStringFunction.php

---
+++ NewStringFunction.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump(str_contains("Nadif Zidan", "Nadif"));
+var_dump(str_contains("Nadif Zidan", "Zidan"));
+var_dump(str_contains("Nadif Zidan", "Joko"));
+
+var_dump(str_starts_with("Nadif Zidan", "Nadif"));
+var_dump(str_starts_with("Nadif Zidan", "Zidan"));
+var_dump(str_starts_with("Nadif Zidan", "Joko"));
+
+var_dump(str_ends_with("Nadif Zidan", "Nadif"));
+var_dump(str_ends_with("Nadif Zidan", "Zidan"));
+var_dump(str_ends_with("Nadif Zidan", "Joko"));
